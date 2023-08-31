8966e746cc589da651c58df6302bde972c772672
Bruce Christie <bqchristie@gmail.com>
2023-08-31 12:10:58 -0400

    Changed the default authentication mechanism to use JWT.   Expiry and Secret keys are configured in the .env file,

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -5,8 +5,6 @@ import urls from "../urls.js";
 import { requireUser } from "../middlewares/auth.js";
 import { requireSchema } from "../middlewares/validate.js";
 import {
-  registerSchema,
-  changePasswordSchema,
   loginSchema,
 } from "../schemas/auth.js";
 
@@ -39,9 +37,9 @@ const router = Router();
  *         description: Bad request, incorrect login credentials
  */
 router.post(urls.auth.login, requireSchema(loginSchema), async (req, res) => {
-  const { email, password } = req.validatedBody;
+  const { username } = req.validatedBody;
 
-  const user = await UserService.authenticateWithPassword(email, password);
+  const user = await UserService.authenticate(username);
 
   if (user) {
     res.json({ user });
@@ -54,49 +52,7 @@ router.get(urls.auth.login, (req, res) => {
   res.status(405).json({ error: "Login with POST instead" });
 });
 
-/** @swagger
- *
- * /auth/register:
- *   post:
- *     tags: [Authentication]
- *     summary: Register with the service
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/registerSchema'
- *     responses:
- *       201:
- *         description: Successful registration, with user details
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *       400:
- *         description: Bad request, registration failed
- */
-router.post(
-  urls.auth.register,
-  requireSchema(registerSchema),
-  async (req, res, next) => {
-    if (req.user) {
-      res.json({ user: req.user });
-      return;
-    }
 
-    try {
-      const user = await UserService.createUser(req.validatedBody);
-      res.status(201).json({ user });
-    } catch (error) {
-      if (error.isClientError()) {
-        res.status(400).json({ error });
-      } else {
-        next(error);
-      }
-    }
-  }
-);
 
 // all auth routes after this can rely on existence of req.user
 router.use(requireUser);
@@ -119,16 +75,5 @@ router.post(urls.auth.logout, async (req, res) => {
 router.get(urls.auth.logout, (req, res) => {
   res.status(405).json({ error: "Logout with POST instead" });
 });
-
-router.post(
-  urls.auth.changePassword,
-  requireSchema(changePasswordSchema),
-  async (req, res) => {
-    const { password } = req.validatedBody;
-
-    await UserService.setPassword(req.user, password.toString());
-    res.status(204).send();
-  }
-);
 
 export default router;
