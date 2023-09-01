203eece65970bab5a4079e0cc72f87b8fcb651f0
Bruce Christie <bqchristie@gmail.com>
2023-09-01 14:22:59 -0400

    Roughed in a socket connection on the server.   The idea is that as queued tasks complete we can push the changes down to the client as the request status changes.

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import http from "http";
+import { Server } from "socket.io";
 
 import app from "./src/app.js";
 import config from "./src/utils/config.js";
 import logger from "./src/utils/log.js";
+import cors from "cors";
 
+app.use(cors());
 const log = logger("server");
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.connected);
+  socket.on("create-request", (data) => {
+    console.log("create-request", data);
+    io.emit("create-request", data);
+  });
+});
 
 process.on("uncaughtException", (err) => {
   log.fatal({ err }, `Unhandled exception ${err}`);
@@ -22,3 +39,6 @@ const main = async () => {
 };
 
 main();
+
+// export io for use in other modules;
+export { io };
