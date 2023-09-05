10024360cbad87023501b420c6052e7a3746b383
Bruce Christie <bqchristie@gmail.com>
2023-09-05 16:28:25 -0400

    quickly changing the port for CORS.  should make this and environment variable.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const log = logger("server");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:8080",
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
   },
 });
